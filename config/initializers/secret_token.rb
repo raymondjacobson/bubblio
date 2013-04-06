895ab7fe54389b0e42974a2076024ad2a8fbfc62
Raymond Jacobson <raymondshujacobson8@gmail.com>
2013-04-06 12:45:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bubblio::Application.config.secret_token = '649af58ae0df445ec55cc72cf57bf13685f8d9c02fc3cb79abaa3608f777308cc1b1a0c1de2215481db9e8ab68434cdb7bf3eb118e7b6820515969e04c5cb52d'

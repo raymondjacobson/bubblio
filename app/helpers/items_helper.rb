a5a24ab9107cc51b0b6669c8e4b22161bc6bce97
Raymond Jacobson <raymondshujacobson8@gmail.com>
2013-04-07 02:29:55 -0400

    fixed download item capabilities, finished drop n drop file upload

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -10,4 +10,15 @@ module ItemsHelper
 		end
 		return ext.reverse
 	end
+	def getFileName(value)
+		name = ""
+		value.reverse.split("").each do |i|
+			if value[i]=="/"
+				break
+			else
+				name = name + value[i]
+			end
+		end
+		return name.reverse
+	end
 end

208f5919a38744b3a336f0e48ac70c5ee2a6cd6a
Ethan Bond <ethanbond@Ethans-MacBook-Pro-2.local>
2013-04-07 06:51:52 -0400

    Fuckdis

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -21,4 +21,22 @@ module ItemsHelper
 		end
 		return name.reverse
 	end
+	def generate_hash(value) #NEEDS WORK STILL
+		randvalue = ((('a'..'z').to_a + ('0'..'9').to_a).shuffle)[0..4].join
+		if ensure_random(randvalue)==false
+			randvalue = ((('a'..'z').to_a + ('0'..'9').to_a).shuffle)[0..4].join
+		end
+		return randvalue;
+	end
+	def ensure_random(value)
+		@bubbles = Bubble.find(:all)
+		@bubbles.each do |bubble|
+			bubble.links.each do |link|
+				if link.link_hash == value
+					return false
+				end
+			end
+		end
+		return true
+	end
 end
